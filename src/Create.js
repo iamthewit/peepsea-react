7f42fb6764fac97ae1e0a5a91d335956999df21f
iamthewit <bencross86@gmail.com>
2020-05-04 20:47:19 +0100

    Added bootstrap and applied some basic styling

--- src/Create.js
+++ src/Create.js
@@ -44,30 +44,56 @@ class CreateForm extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div>
+            <div className="container">
+            <form
+                onSubmit={this.handleSubmit}
+            >
+                <h1>Create A New PeepSea!</h1>
+                    <label>Answer:</label>
+                    <input
+                        type="text"
+                        name="answer"
+                        value={this.state.answer} onChange={this.handleAnswerChange}
+                        className="form-control"
+                        placeholder="The quick brown fox jumps over the lazy dog"
+                    />
+                    <p>Image Paths:</p>
                     {this.state.images.map((image, index) => (
-                        <div>
-                            <label>
-                                Image:
-                                <input type="text" value={image.path} onChange={this.handleImagePathChange(index)} />
-                            </label>
-
-                            <button type="button" onClick={this.handleRemoveImagePath(index)}>Remove Image</button>
+                        <div className="form-row">
+                            <div className="form-group col-md">
+                                <input
+                                    type="text"
+                                    value={image.path}
+                                    onChange={this.handleImagePathChange(index)}
+                                    className="form-control"
+                                    placeholder="https://www.some-image-website.com/path/to/image.png"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <button
+                                    type="button"
+                                    onClick={this.handleRemoveImagePath(index)}
+                                    className="btn btn-warning"
+                                >
+                                    Remove Image
+                                </button>
+                            </div>
                         </div>
                     ))}
-                    <label>
-                        Answer:
-                        <input type="text" name="answer" value={this.state.answer} onChange={this.handleAnswerChange}/>
-                    </label>
+                <div className="form-group">
+                    <button
+                        type="button"
+                        onClick={this.handleAddImagePath}
+                        className="btn btn-secondary"
+                    >
+                        Add Image Path
+                    </button>
                 </div>
-                <div>
-                    <button type="button" onClick={this.handleAddImagePath}>Add Image Path</button>
-                </div>
-                <div>
-                    <input type="submit" value="Create PeepSea!" />
+                <div className="form-group">
+                    <input type="submit" value="Create PeepSea!" className="btn btn-primary"/>
                 </div>
             </form>
+            </div>
         )
     }
 }
